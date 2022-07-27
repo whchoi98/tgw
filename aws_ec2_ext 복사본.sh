7045ba338e2ee0504cedc0d59053ac2a874f7f7e
WOOHYUNG CHOI <whchoi98@gmail.com>
2022-07-28 01:04:45 +0900

    add yaml - aws_ec2_ext_nrt.sh

--- aws_ec2_ext 복사본.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# command aws_ec2.sh
-aws ec2 describe-instances --query 'Reservations[].Instances[].[Tags[?Key==`Name`] | [0].Value, Placement.AvailabilityZone,InstanceId, InstanceType, ImageId,State.Name, PrivateIpAddress, PublicIpAddress ]' --output table
