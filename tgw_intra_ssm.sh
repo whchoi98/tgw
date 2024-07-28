b33a577c982ed665a8e2b69689d1281952dc1327
whchoi98 <whchoi98@gmail.com>
2024-07-28 17:10:11 +0900

    tgw_inra_ssm.sh

--- tgw_intra_ssm.sh
+++ tgw_intra_ssm.sh
@@ -5,9 +5,9 @@ export Seoul_VPC_PART_PRD_Private_10_11_21_101=$(aws ec2 describe-instances --fi
 echo "export Seoul_VPC_PART_PRD_Private_10_11_21_101=${Seoul_VPC_PART_PRD_Private_10_11_21_101}"| tee -a ~/.bash_profile
 
 aws ec2 describe-instances --filters 'Name=tag:Name,Values=Seoul-VPC-PART-STG-Private-10.12.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
-export Seoul_VPC_PART_PRD_Private_10_12_21_101=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=Seoul-VPC-PART-STG-Private-10.12.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
-echo "export Seoul_VPC_PART_PRD_Private_10_12_21_101=${Seoul_VPC_PART_PRD_Private_10_12_21_101}"| tee -a ~/.bash_profile
+export Seoul_VPC_PART_STG_Private_10_12_21_101=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=Seoul-VPC-PART-STG-Private-10.12.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
+echo "export Seoul_VPC_PART_STG_Private_10_12_21_101=${Seoul_VPC_PART_STG_Private_10_12_21_101}"| tee -a ~/.bash_profile
 
 aws ec2 describe-instances --filters 'Name=tag:Name,Values=Seoul-VPC-PART-DEV-Private-10.13.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
-export Seoul_VPC_PART_STG_Private_10_13_21_101=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=Seoul-VPC-PART-DEV-Private-10.13.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
-echo "export Seoul_VPC_PART_STG_Private_10_13_21_101=${Seoul_VPC_PART_STG_Private_10_13_21_101}"| tee -a ~/.bash_profile
+export Seoul_VPC_PART_DEV_Private_10_13_21_101=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=Seoul-VPC-PART-DEV-Private-10.13.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
+echo "export Seoul_VPC_PART_DEV_Private_10_13_21_101=${Seoul_VPC_PART_DEV_Private_10_13_21_101}"| tee -a ~/.bash_profile
